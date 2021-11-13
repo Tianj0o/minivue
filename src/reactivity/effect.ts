fca50ca6b37a8370a3d561ecd0973b678e388f98
Tianj0o <2576099053@qq.com>
2021-11-13 22:48:15 +0800

    scheduler&&stop&&onStop

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,13 +1,36 @@
+import { extend } from "../shared";
+
 class effectFn {
   _fn: any;
-  constructor(fn) {
+  scheduler?: any;
+  deps: any[] = [];
+  active = true;
+  onStop?: () => void;
+  constructor(fn, scheduler?) {
     this._fn = fn;
+    this.scheduler = scheduler;
   }
   run() {
     activeFn = this;
     return this._fn();
   }
+  stop() {
+    if (this.active) {
+      if (this.onStop) {
+        this.onStop();
+      }
+      clearupEffect(this);
+      this.active = false;
+    }
+  }
 }
+
+function clearupEffect(effect) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect);
+  });
+}
+
 let activeFn;
 export const targetMap = new Map();
 
@@ -22,15 +45,32 @@ export function track(target, key) {
     effectFnSet = new Set();
     effectMap.set(key, effectFnSet);
   }
+  if (!activeFn) return;
   effectFnSet.add(activeFn);
+
+  //给activeFn添加 收集到的set stop功能
+  activeFn.deps.push(effectFnSet);
 }
 export function trigger(target, key) {
   const effectMap = targetMap.get(target);
   const effectFns = effectMap.get(key);
-  effectFns.forEach((fn) => fn.run());
+  effectFns.forEach((effect) => {
+    if (effect.scheduler) {
+      effect.scheduler();
+    } else {
+      effect._fn();
+    }
+  });
 }
-export const effect = function (fn) {
-  const effect = new effectFn(fn);
+export const effect = function (fn, options: any = {}) {
+  const effect = new effectFn(fn, options.scheduler);
+  extend(effect, options);
   effect.run();
-  return effect.run.bind(effect);
+  const runner: any = effect.run.bind(effect);
+  runner.effect = effect;
+  return runner;
+};
+
+export const stop = (runner) => {
+  runner.effect.stop();
 };
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -1,5 +1,5 @@
 import { reactive } from "../reactive";
-import { effect } from "../effect";
+import { effect, stop } from "../effect";
 describe("happy path", () => {
   it("happy path", () => {
     const user = reactive({ age: 1 });
@@ -22,5 +22,65 @@ describe("happy path", () => {
     const res = runner();
     expect(res).toBe("hhhh");
     expect(num).toBe(2);
+  });
+  it("scheduler", () => {
+    let dummy;
+    let run: any;
+    const scheduler = jest.fn(() => {
+      run = runner;
+    });
+    const obj = reactive({ foo: 1 });
+    const runner = effect(
+      () => {
+        dummy = obj.foo;
+      },
+      { scheduler }
+    );
+    expect(scheduler).not.toHaveBeenCalled();
+    expect(dummy).toBe(1);
+    // should be called on first trigger
+    obj.foo++;
+    expect(scheduler).toHaveBeenCalledTimes(1);
+    // // should not run yet
+    expect(dummy).toBe(1);
+    // // manually run
+    run();
+    // // should have run
+    expect(dummy).toBe(2);
+  });
+
+  it("stop", () => {
+    let dummy;
+    const obj = reactive({ prop: 1 });
+    const runner = effect(() => {
+      dummy = obj.prop;
+    });
+    obj.prop = 2;
+    expect(dummy).toBe(2);
+    stop(runner);
+    obj.prop = 3;
+    expect(dummy).toBe(2);
+
+    // stopped effect should still be manually callable
+    runner();
+    expect(dummy).toBe(3);
+  });
+  it("onStop", () => {
+    const obj = reactive({
+      foo: 1,
+    });
+    const onStop = jest.fn();
+    let dummy;
+    const runner = effect(
+      () => {
+        dummy = obj.foo;
+      },
+      {
+        onStop,
+      }
+    );
+
+    stop(runner);
+    expect(onStop).toBeCalledTimes(1);
   });
 });
