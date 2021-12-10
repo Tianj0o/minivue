88f06fcf3d8ec2c6aea3f8e67f34a10fdfb07f5d
Tianj0o <2576099053@qq.com>
2021-12-10 15:25:29 +0800

    使用rollup&&实现初始化element

--- example/hellowrold/App.js
+++ example/hellowrold/App.js
@@ -1,6 +1,15 @@
+import { h } from '../../lib/mini-vue.esm.js'
 export const App = {
   render() {
-    return h('div', 'hello,' + this.msg)
+    return h('div', {
+      id: "test",
+      class: 'red test'
+    },
+      //string
+      // 'hello,' + this.msg
+      //array
+      [h('p', { class: 'red' }, 'hhhhh' + this.msg), h('p', { class: 'blue' }, 'bbbb' + this.msg)]
+    )
   },
   setup() {
     return {
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,13 +1,46 @@
+import { isObject } from "../shared/index";
 import { createComponentInstance, setupComponent } from "./component";
 
 export function render(vnode, container) {
   patch(vnode, container);
 }
 function patch(vnode: any, container: any) {
-  //处理组件
-  processComponent(vnode, container);
+  //通过vnode.type 判断这个vnode是组件 还是 htmlElement
+  if (typeof vnode.type === "string") {
+    //处理元素
+    processElement(vnode, container);
+  } else if (isObject(vnode.type)) {
+    //处理组件
+    processComponent(vnode, container);
+  }
 }
+function processElement(vnode: any, container: any) {
+  mountElement(vnode, container);
+}
+function mountElement(vnode: any, container: any) {
+  // vnode
+  const el = document.createElement(vnode.type);
 
+  const { children } = vnode;
+  if (typeof children === "string") {
+    el.textContent = vnode.children;
+  } else if (Array.isArray(children)) {
+    mountChildren(children, el);
+  }
+
+  //props {}
+  Object.keys(vnode.props).forEach((key) => {
+    const val = vnode.props[key];
+    el.setAttribute(key, val);
+  });
+
+  container.append(el);
+}
+function mountChildren(vnode, container) {
+  vnode.forEach((v) => {
+    patch(v, container);
+  });
+}
 function processComponent(vnode: any, container: any) {
   //挂载组件
   mountComponent(vnode, container);
@@ -23,7 +56,7 @@ function mountComponent(vnode: any, container) {
   setupRenderEffect(instance, container);
 }
 function setupRenderEffect(instance: any, container: any) {
-  const subTree = instance.render();
+  const subTree = instance.render.call(instance.setupState);
 
   patch(subTree, container);
 }
